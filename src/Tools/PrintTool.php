e83a32c5a444d52ae96d7fa491fb1e182235b364
Youssef Jaber <youssefjbr@hotmail.com>
2025-06-30 12:05:26 +0300

    V1

--- src/Tools/PrintTool.php
+++ src/Tools/PrintTool.php
@@ -1,20 +1,30 @@
 <?php
+
 namespace ManiaLab\ManiaPrintLab\Tools;
 
-use Laravel\Nova\Nova;
 use Laravel\Nova\Tool;
+use Laravel\Nova\Nova;
+use Laravel\Nova\Menu\MenuSection;
+use Illuminate\Http\Request; // ← correct import
 
 class PrintTool extends Tool
 {
+    /**
+     * Bootstrap any application services.
+     */
     public function boot()
     {
-        Nova::script('maniaprintlab-print', __DIR__.'/../../resources/printlab/js/tool.js');
+        // If you have JS to load for your tool, otherwise you can remove this line
+        Nova::script('maniaprintlab-print', __DIR__ . '/../../resources/printlab/js/tool.js');
     }
 
+    /**
+     * Build the menu for the Nova sidebar.
+     */
     public function menu(Request $request)
     {
         return MenuSection::make('Print Badges')
-            ->path('/maniaprintlab/print')
+            ->path('/nova-vendor/maniaprintlab/print')  // make sure this matches your route
             ->icon('printer');
     }
 }
